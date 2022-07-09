751d8a9ccccfd533bd9925ba29f96260ec8a435c
mahesh-248 <83508657+mahesh-248@users.noreply.github.com>
2022-07-09 20:46:09 +0530

    Update web.py

--- web.py
+++ web.py
@@ -11,6 +11,7 @@ def fetch_poster(movie_id):
     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
     return full_path
 
+
 # function to return movie names and poster of top five similar movies got through model we built
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
@@ -25,6 +26,8 @@ def recommend(movie):
 
     return recommended_movie_names,recommended_movie_posters
 
+
+
 # web UI
 st.header('Movie Recommender System')
 movies = pickle.load(open('movie_list.pkl','rb'))
@@ -36,6 +39,8 @@ selected_movie = st.selectbox(
     movie_list
 )
 
+
+
 # shows the recommended movies
 if st.button('Show Recommendation'):
     recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
@@ -43,16 +48,19 @@ if st.button('Show Recommendation'):
     with col1:
         st.text(recommended_movie_names[0])
         st.image(recommended_movie_posters[0])
+        
     with col2:
         st.text(recommended_movie_names[1])
         st.image(recommended_movie_posters[1])
-
+        
     with col3:
         st.text(recommended_movie_names[2])
         st.image(recommended_movie_posters[2])
+        
     with col4:
         st.text(recommended_movie_names[3])
         st.image(recommended_movie_posters[3])
+        
     with col5:
         st.text(recommended_movie_names[4])
         st.image(recommended_movie_posters[4])
